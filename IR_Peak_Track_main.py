7f4b3425aa19cc4acbce4cd65d34f89b398bb96b
Anthony <89137593+taekwon101@users.noreply.github.com>
2022-04-28 14:04:23 -0400

    Update IR_Peak_Track_main.py

--- IR_Peak_Track_main.py
+++ IR_Peak_Track_main.py
@@ -7,7 +7,7 @@ from scipy.integrate import simps
 import glob, os
 
 # assign file paths/names
-readpath = r'C:\Users\taekw\Desktop\1_PythonScripts\IRPeakExtract\CSVs\220418_808nm_3p5A_5e-5cbPDMS'
+readpath = r'C:\Users\taekw\Desktop\1_PythonScripts\IRPeakExtract\CSVs\220428_cbPDMS_3A_5e-4'
 writepath = r'C:\Users\taekw\Desktop\1_PythonScripts\IRPeakExtract\CSVs\Output'
 output = '220317_5e7 cbPDMS_laser 10A oven 100C_abstest.csv'
 willExport = False
@@ -17,27 +17,27 @@ filelist = sorted(glob.glob('*.csv')) #make list of names of csvs from readpath 
 
 # normalization and baseline correction wavenumbers
 WN_normal_CH = 2960
-WN_baseline_low = 3200
-WN_baseline_high = 3400
+WN_baseline_low = 3400 
+WN_baseline_high = 3600
 
 # wavenumber range to integrate over for bar graph (see info.txt for functional groups but 940 - 1230 seems best)
 WN_low = 940
 WN_high = 1230
 
 # select type of plot to show (true = scatter, false = bar)
-plotScatter = True
+plotScatter = False
 
 # matplot manual formatting/scaling values
 manual_y = True
-ymin = -1
-ymax = 5
-xmin = 700
-xmax = 3000
+ymin = -2.5
+ymax = 2.5
+xmin = 900
+xmax = 1300
 width = 0.8 # primary bar plot bar width
-plotSize = 5 # size of dots in scatterplot
+plotSize = 10 # size of dots in scatterplot
 
 # assign colors for differentiating overlapping spectra in scatterplots (currently alternates between 6 color-blind-friendly colors)
-colorlist = ['#d55e00', '#cc79a7', '#0072b2', '#f0e442', '#009e73', '#24ff24']#, '#000000","#004949","#009292","#ff6db6","#ffb6db","#490092","#006ddb","#b66dff","#6db6ff","#b6dbff","#920000","#924900","#db6d00","#24ff24","#ffff6d]
+colorlist = ['#cd6155', '#ec7063', '#af7ac5', '#a569bd', '#5499c7', '#5dade2', '#48c9b0', '#45b39d', '#52be80', '#58d68d', '#f4d03f', '#f5b041'] #'eb984e', '#dc7633', '#f0f3f4', '#cacfd2', '#aab7b8', '#99a3a4', '#5d6d7e', '#566573']#['#d55e00', '#cc79a7', '#0072b2', '#f0e442', '#009e73', '#24ff24','#000000","#004949","#009292","#ff6db6","#ffb6db","#490092","#006ddb","#b66dff","#6db6ff","#b6dbff","#920000","#924900","#db6d00","#24ff24","#ffff6d]
 colorlength = len(colorlist)
 
 # prepare dataframes/arrays/variables for loop
@@ -74,22 +74,27 @@ for item in WN_array:
         index_baseline_low += 1
     if item < WN_baseline_high:
         index_baseline_high += 1
-    
+
 # append signal columns from each CSV in directory to the intial dataframe stepwise in a loop
 for file in filelist:     
     columnname = file[0:-4] # extract column name (slicing off '.csv' from filename)
     df_temp = pd.read_csv(file, names = ['wavenumber', columnname]) # temp dataframe with csv
     normal_temp = df_temp.iloc[index_normal_CH][columnname] # signal value for current csv at normalization index
-    baseline_temp = df_temp.iloc[WN_baseline_low:WN_baseline_high][columnname].mean(axis = 0) # baseline correction value to subtract
-    
+    #baseline_temp = df_temp.iloc[index_baseline_low:index_baseline_high][columnname].mean(axis = 0) # baseline correction value to subtract
+    baseline_temp = df_temp.iloc[index_baseline_low][columnname] # baseline correction value to subtract
+
     # transmittance-to-absorbance conversion (on condition that it is not already in absorbance)
     if isAbs == False:
         df_temp[columnname] /= 100
         df_temp[columnname] += 1e-10 #bandaid for 'log of 0' errors
         df_temp[columnname] = np.log10(df_temp[columnname]) * -1
+        
+    # baseline subtraction # broken for transmittance for now, come back to
+    #df_temp[columnname] -= baseline_temp
     
-    # baseline subtraction
-    df_temp[columnname] -= baseline_temp
+    #print(df_temp)
+    print(index_baseline_high)
+    print(df_temp.iloc[index_baseline_high][columnname])
     
     # normalization
     df_temp[columnname] /= normal_temp 
@@ -149,4 +154,5 @@ plt.show()
 # export (arrays to DF to CSV)
 if willExport == True:
     df_export = pd.DataFrame({"sample (cor./norm.)" : namearray, "area" : areaarray})
-    df_export.to_csv("output.csv", index = False)
+    df_export.to_csv(output, index = False)
+    
