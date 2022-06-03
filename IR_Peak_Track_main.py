dd0818d2a9c5e0f4a8763757a938ed7e374ff61c
Anthony <89137593+taekwon101@users.noreply.github.com>
2022-06-03 15:53:40 -0400

    Update IR_Peak_Track_main.py

--- IR_Peak_Track_main.py
+++ IR_Peak_Track_main.py
@@ -1,19 +1,24 @@
-from math import floor
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from scipy import stats
 from scipy.integrate import simps
 import glob, os
+from pathlib import Path
+from scipy import stats
+from math import floor
 
 # assign file paths/names
-readpath = r'C:\Users\taekw\Desktop\1_PythonScripts\IRPeakExtract\CSVs\220504_808nm_5A_5e-5cbPDMS'
-writepath = r'C:\Users\taekw\Desktop\1_PythonScripts\IRPeakExtract\CSVs\Output\220504_808nm_5A_5e-5cbPDMS.csv'
-output = '220504_808nm_5A_5e-5cbPDMS.csv'
+colortext = open('colorlist.txt')
+readpath = r'CSVs\220601_cbPDMS_RTLaser_underwaterreal'
+writepath = r'C:\Users\taekw\Desktop\1_PythonScripts\IRPeakExtract\CSVs\Output'
+output = '220513_cbPDMS_laserrots_oven.csv'
 willExport = False
 
-os.chdir(readpath) #change working directory to folder with CSVs of interest
-filelist = sorted(glob.glob('*.csv')) #make list of names of csvs from readpath directory  
+# change working directory to folder with CSVs of interest
+os.chdir(readpath) 
+
+# make list of names of csvs from readpath directory 
+filelist = sorted(glob.glob('*.csv'))  
 
 # normalization and baseline correction wavenumbers
 WN_normal_CH_low = 2957
@@ -21,22 +26,25 @@ WN_normal_CH_high = 2967
 WN_baseline_low = 3400 
 WN_baseline_high = 3600
 
+# assign control number (loop will average over this number of initial areas for control %change-from value)
+control_number = 5
+
 # choose wavenumber range to integrate over for bar graph
 WN_group = 5
 
-if WN_group == 1: # (Si-O-Si?): more cure --> ? signal (this and 2 need more thinking but )
+if WN_group == 1: # (Si-O-Si ?): more cure --> ? signal (this and 2 need more thinking but)
     WN_low = 715
     WN_high = 830
-    groupname = 'Si-O-Si'
-elif WN_group == 2: # (Si-O-Si): more cure --> ? signal (this and 1 need more thinking)
+    groupname = 'Si-O-Si (?)'
+elif WN_group == 2: # (Si-O-Si ?): more cure --> ? signal (this and 1 need more thinking)
     WN_low = 940
     WN_high = 1230
-    groupname = 'Si-O-Si'
+    groupname = 'Si-O-Si (?)'
 elif WN_group == 3: # silane (Si-H): more cure --> LOWER signal (often very small with ATR)
     WN_low = 2290
     WN_high = 2390
     groupname = 'Si-H'
-elif WN_group == 4: # (-CH2- and -CH3): more cure --> SAME signal (for checking if internal standards are changing)
+elif WN_group == 4: # (-CH2- and -CH3 ?): more cure --> SAME signal (for checking if internal standards are changing)
     WN_low = 2900
     WN_high = 2970
     groupname = 'CH3'
@@ -46,29 +54,24 @@ elif WN_group == 5: # vinyl (CH=CH2) asym.: more cure --> LOWER signal (often ve
     groupname = 'vinyl'
     
 # select type of plot to show (true = scatter, false = bar)
-plotScatter = False
+plotScatter = True
+plotkind = 'line' #line, bar, barh, hist, box, scatter
 
-# matplot manual formatting/scaling values
-manual_y = False
-ymin = -1
-ymax = 1
-xmin = 300
+# matplot formatting/scaling values
+xmin = 600
 xmax = 4000
 width = 0.8 # primary bar plot bar width
-plotSize = 5 # size of dots in scatterplot
-
-# assign control number (loop will average over this number of initial areas for control %change-from value)
-control_number = 10
 
 # assign colors for differentiating overlapping spectra in scatterplots (currently cycles across custom contrast gradient)
-colorlist = ['#cd6155', '#ec7063', '#af7ac5', '#a569bd', '#5499c7', '#5dade2', '#48c9b0', '#45b39d', '#52be80', '#58d68d', '#f4d03f', '#f5b041'] #'eb984e', '#dc7633', '#f0f3f4', '#cacfd2', '#aab7b8', '#99a3a4', '#5d6d7e', '#566573']#['#d55e00', '#cc79a7', '#0072b2', '#f0e442', '#009e73', '#24ff24','#000000","#004949","#009292","#ff6db6","#ffb6db","#490092","#006ddb","#b66dff","#6db6ff","#b6dbff","#920000","#924900","#db6d00","#24ff24","#ffff6d]
+colorlist = colortext.read().split()
 colorlength = len(colorlist)
 
 # prepare dataframes/arrays/variables for loop
 df_IR_total = pd.read_csv(filelist[0], names = ['wavenumber','temp']) # read first CSV in designated folder and write wavenumbers to primer dataframe and an array
-isAbs = False # check if CSVs are for absorbance or transmittance signals before primer dataframe drops signal column
-if df_IR_total['temp'].max() < 20:
+if df_IR_total['temp'].max() < 20: # check if CSVs are for absorbance or transmittance signals before primer dataframe drops signal column
     isAbs = True
+else:
+    isAbs = False
 df_IR_total = df_IR_total.drop(['temp'], axis = 1)
 WN_array = np.array(df_IR_total['wavenumber'])
 df_IR_total.set_index('wavenumber', append = False, inplace = True)
@@ -77,7 +80,7 @@ namearray = []
 areaarray = []
 changearray = []
 area_control = 0
-colorcount = colorlength # start at max value for modulo loop count
+colorcount = colorlength # starts at max value for modulo loop count
 ax = plt.gca() # define constant axis for iterable additions
 
 # find indices that correspond to integration bounding and normalization wavenumbers so they can be referenced directly
@@ -87,18 +90,18 @@ index_normal_CH_low = 0
 index_normal_CH_high = 0
 index_baseline_low = 0
 index_baseline_high = 0
-for item in WN_array:
-    if item < WN_low:
+for WN in WN_array:
+    if WN < WN_low:
         index_low += 1
-    if item < WN_high:
+    if WN < WN_high:
         index_high += 1
-    if item < WN_normal_CH_low:
+    if WN < WN_normal_CH_low:
         index_normal_CH_low += 1
-    if item < WN_normal_CH_high:
+    if WN < WN_normal_CH_high:
         index_normal_CH_high += 1
-    if item < WN_baseline_low:
+    if WN < WN_baseline_low:
         index_baseline_low += 1
-    if item < WN_baseline_high:
+    if WN < WN_baseline_high:
         index_baseline_high += 1
 
 # append signal columns from each CSV in directory to the intial dataframe stepwise in a loop
@@ -112,7 +115,7 @@ for file in filelist:
         df_temp[columnname] += 1e-10 #bandaid for 'log of 0' errors
         df_temp[columnname] = np.log10(df_temp[columnname]) * -1
         
-    # baseline subtraction (breaks sometimes and makes values negative?)
+    # baseline subtraction
     baseline_temp = df_temp.iloc[index_baseline_low:index_baseline_high][columnname].mean(axis = 0) # baseline correction value to subtract
     df_temp[columnname] -= baseline_temp
 
@@ -125,7 +128,7 @@ for file in filelist:
     
     # superimpose corrected IR data with cycling color to existing axis for scatterplot
     if plotScatter == True:
-        df_temp.plot(kind = 'scatter', x = 'wavenumber', y = columnname, s = plotSize, c = colorlist[colorcount % colorlength], label = columnname, ax = ax)
+        df_temp.plot(kind = plotkind, x = 'wavenumber', y = columnname, c = colorlist[colorcount % colorlength], label = columnname, ax = ax) # old: s = plotSize, c = colorlist[colorcount % colorlength]
     colorcount += 1 # out of conditional because also serves as loop counter below
     
     # peak integration, baseline subtraction, and array collection of values for bar graph (ty Nate and Sarah)
@@ -139,7 +142,6 @@ for file in filelist:
         namearray.append(columnname)
         areaarray.append(area - baseline_area)
 
-        
         # find % change values to populate second bar graph (starting with checking if first time through loop for control value)
         if colorcount <= colorlength + control_number:
             area_control += (area - baseline_area)
@@ -152,8 +154,6 @@ for file in filelist:
 
 # create scatterplot
 if plotScatter == True:
-    if manual_y == True:
-        plt.ylim((ymin, ymax))
     plt.xlim((xmin, xmax))
     x_label = ('wavenumbers /cm-1')
     plt.xlabel(x_label)
@@ -161,10 +161,10 @@ if plotScatter == True:
     plt.ylabel(y_label)
     plt.title("{} IR spectra from {}-{} cm-1".format(os.path.basename(os.path.normpath(readpath)), xmin, xmax)) # uses folder name as plot title
 
+#df_IR_total.plot(kind = plotkind, y = df_IR_total.columns, colormap = 'tab20') # old: y = columnname, s = plotSize, c = colorlist[colorcount % colorlength]
+
 # create bar graph
 if plotScatter == False:
-    if manual_y == True:
-        plt.ylim((ymin, ymax))
     plt.bar(namearray, areaarray, width = width, color = 'b', label = 'Integrated Signals')
     plt.bar(namearray, changearray, width = 0.5 * width, color = 'r', alpha = 0.8, label = 'percent change')
     y_label = 'integrated signal' #('integrated signal from {WN_low} cm-1 to {WN_high} cm-1')
